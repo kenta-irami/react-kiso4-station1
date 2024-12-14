4b839f31ac7c5f800da645257a40bb77db59e98d
KentaIramina <iramina_kenta@yahoo.co.jp>
2024-12-15 05:41:41 +0900

    アイコン追加

--- src/PersonalIntro.tsx
+++ src/PersonalIntro.tsx
@@ -1,23 +1,46 @@
 import React from "react";
 import "./PersonalIntro.css";
+import profileImage from "./assets/profile.jpg";
+import {
+  FaWordpress,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaReact,
+  FaUser,
+} from "react-icons/fa";
 
 const PersonalIntro: React.FC = () => {
   return (
     <div className="personal-intro">
-      <h1>伊良皆 健太</h1>
-      <p className="occupation">Web制作</p>
+      <div className="header">
+        <img src={profileImage} alt="プロフィール画像" className="user-icon" />
+        <h1>イラケン</h1>
+      </div>
+      <p className="occupation">Webページ、ホームページ制作</p>
 
       <p className="description">
-        Webライティング＆プログラミングスキルを身につけることが私の目標です。
-        <br></br>常に新しいことを学び、成長し続けています。
+        ノマドワーカーとして自由に生きることが夢です。
+        <br />
+        スキルを身につけることと同時に、人間的にも成長していきます。
       </p>
 
       <div className="skills">
-        {["HTML", "CSS", "JavaScript", "React"].map((skill) => (
-          <span key={skill} className="skill-tag">
-            {skill}
-          </span>
-        ))}
+        <span className="skill-tag">
+          <FaWordpress /> Wordpress
+        </span>
+        <span className="skill-tag">
+          <FaHtml5 /> HTML
+        </span>
+        <span className="skill-tag">
+          <FaCss3Alt /> CSS
+        </span>
+        <span className="skill-tag">
+          <FaJs /> JavaScript
+        </span>
+        <span className="skill-tag">
+          <FaReact /> React
+        </span>
       </div>
     </div>
   );
